a9bfef1073c455854e0d0b06a30046f8e49b2a34
Roel de Jong <Roel4811@hotmail.com>
2016-11-07 17:13:25 +0100

    fix syntax mistake

--- app/models/barber.rb
+++ app/models/barber.rb
@@ -1,4 +1,4 @@
 class Barber < ApplicationRecord
   has_many :appointments, :reviews
-  validates :name, :description, :services, :location, presence:true
+  validates :name, :description, :services, :location, presence: true
 end
