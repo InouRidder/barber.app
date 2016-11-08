ba540eaa78c9eb58515fef601052e051d04e541f
Ollie Senduk <ollie_senduk@hotmail.com>
2016-11-08 11:22:54 +0100

    ChangedValidationNameforUser

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :appointments
   has_many :reviews
-  validates :name, :email, presence: true
+  validates :email, presence: true
 end
