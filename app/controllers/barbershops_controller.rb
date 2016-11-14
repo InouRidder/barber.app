46c9fa9da2f01ce3ca4fbcc309a40320ee00ae96
Roel de Jong <Roel4811@hotmail.com>
2016-11-14 16:09:28 +0100

    add barbershop controller and view

--- app/controllers/barbershops_controller.rb
+++ app/controllers/barbershops_controller.rb
@@ -1,3 +1,20 @@
 class BarbershopsController < ApplicationController
 
+before_action :find_barbershop, only: [:index]
+
+  def index
+
+  end
+
+  private
+
+  def find_barbershop
+    @barbershop = Barbershop.find(params[:id])
+  end
+
+  def barber_params
+    params.require(:barbershop).permit(:name, :description, :location)
+  end
+
+
 end
