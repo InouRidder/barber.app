ee7cab9223e4d9df61d6759312b526c7900182bb
Roel de Jong <Roel4811@hotmail.com>
2016-11-19 18:51:41 +0100

    barber model distance

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,5 +1,7 @@
 require 'date'
 require 'time'
+require 'open-uri'
+require 'json'
 
 
 class AvailabilitiesController < ApplicationController
@@ -15,18 +17,29 @@ class AvailabilitiesController < ApplicationController
   end
 
   def index
-    lat = params[:lat]
-    lng = params[:lng]
-    availablities = Availability.all
-    @available_slots = []
-    availablities.each do |element|
-      if element.end - 40.minutes >= DateTime.now
-    @available_slots << element
-      end
-    end
-   @available_slots
-  end
+    @availabilities = Availability.all
+    # Availability.where()  << don't use ruby to select data, but an AR query like this
+    @lat = params[:lat]
+    @lng = params[:lng]
+    @barbers = Availability.select(:barber_id).distinct
 
+   #  @coordinates = Gmaps4rails.build_markers(@barbers) do |barber, marker|
+   #   marker.lat barber.latitude
+   #   marker.lng barber.longitude
+   #   marker.infowindow render_to_string(partial: "/barbers/map_box", locals: { barber: barber })
+   # end
+ end
+
+  # def available
+  #   availabilities = Availability.all
+  #   available_slots = []
+  #   availabilities.each do |element|
+  #     # if element.end - 40.minutes >= DateTime.now
+  #   available_slots << element
+  #     # end
+  #   end
+  #   return available_slots
+  # end
 
   private
 
