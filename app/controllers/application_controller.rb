787bbf4ae18285e88a7a306c0ab0d8d5ff609abb
Roel de Jong <Roel4811@hotmail.com>
2016-11-24 14:27:53 +0100

    add meta stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:role])
   end
+
+  def default_url_options
+    {Host: ENV [ 'HOST'] || 'localhost: 3000'}
+  end
 end
